6d060b33759303dd4a7323415d20608d48e77981
claire601 <clacha24@bergen.org>
2024-11-09 18:48:14 -0500

    BOOM

--- Sudi.java
+++ Sudi.java
@@ -28,70 +28,59 @@ public class Sudi {
         currStates.add(start);
         currScores.put(start, 0.0);
 
-        for (int i = 0; i < splitLine.length - 1; i++){
+        for (int i = 0; i < splitLine.length - 1; i++){ //for each word
             nextScores = new HashMap<>();
             nextStates = new HashSet<>();
 
-            for (String currState : currStates){
-                for (String transition : BuildModel.getTransitions().get(currState).keySet()){
-                    nextStates.add(transition);
+            for (String currState : currStates){ //for each current state
+                //for each next state (current state to each next state: transition)
+                for (String nextState : BuildModel.getTransitions().get(currState).keySet()){
+                    nextStates.add(nextState); //add next state
                     double nextScore = 0;
-                    nextScore += BuildModel.getTransitions().get(currState).get(transition);
+                    //add transition score
+                    nextScore += BuildModel.getTransitions().get(currState).get(nextState);
+                    //add score corresponding to curr state
                     nextScore += currScores.get(currState);
+
+                    //if curr state observations don't have an instance of the word
                     if (!BuildModel.getObservations().get(currState).containsKey(splitLine[i])){
-                        nextScore += unseenScore;
+                        nextScore += unseenScore; //add unseen score
                     }
                     else {
+                        //add score corresponding to word
                         nextScore += BuildModel.getObservations().get(currState).get(splitLine[i]);
                     }
 
-                    if (!nextScores.containsKey(transition) || nextScore > nextScores.get(transition)){
-                        nextScores.put(transition, nextScore);
-                        track.put(transition, currState);
+                    //if there is no score for next state or the score now is greater than the previous one for next state
+                    if (!nextScores.containsKey(nextState) || nextScore > nextScores.get(nextState)){
+                        //add next state and score
+                        nextScores.put(nextState, nextScore);
+                        //keep track of next state and state it came from
+                        track.put(nextState, currState);
                     }
-                    currStates = nextStates;
-                    currScores = nextScores;
+                    currStates = nextStates; //next states are used for new current
+                    currScores = nextScores; //next scores are used for new current
                 }
-
             }
-            trackList.add(track); //add observation per word
+            trackList.add(track); //add observation per word in line
         }
 
-        //for each unique next state
+        String state = "";
+        double max = -1;
+        //for each unique next state for the last word
         for (String s : trackList.get(trackList.size() - 1).keySet()){
-            double max = -1;
-            String state = "";
             if (currScores.get(s) > max){
                 max = currScores.get(s);
-                state = s;
+                state = s; //state with the highest score
             }
         }
-
-
-
-
-
-        //String[] splitLine = line.split(" ");
-        String[] sentence = new String[splitLine.length + 1];
-        for (int i = 1; i < splitLine.length; i++){
-            sentence[i] = splitLine[i - 1];
+        backtrace.add(state); //add state with highest score
+        for (int i = trackList.size() - 1; i >= 0; i--){
+            //add the current state that corresponds to next state
+            backtrace.add(trackList.get(i).get(state));
+            //set state to current state
+            state = trackList.get(i).get(state);
         }
-        sentence[0] = start;
-
-//        for (String s : sentence){
-//            double score = 0;
-//                for (String tag : BuildModel.getObservations().keySet()){
-//                    if (!BuildModel.getObservations().get(tag).containsKey(s)){
-//                        score += unseenScore;
-//                    }
-//                }
-//
-//
-//
-//        }
-
-
-
     }
 
 
