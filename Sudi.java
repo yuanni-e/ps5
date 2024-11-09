d90997ec6e0e3924acee208ae9d85cbad6cd73e2
claire601 <clacha24@bergen.org>
2024-11-09 18:06:34 -0500

    they know me as the rizzler i'm the panther dressed in black once i give your girl the rizz face there's no chance she's coming back

--- Sudi.java
+++ Sudi.java
@@ -24,47 +24,54 @@ public class Sudi {
 
     public void POSViterbi(String line){
         String[] splitLine = line.split(" ");
-//        String[] sentence = new String[splitLine.length + 1];
-//        for (int i = 1; i < splitLine.length; i++){
-//            sentence[i] = splitLine[i - 1];
-//        }
-//        sentence[0] = start;
 
         currStates.add(start);
         currScores.put(start, 0.0);
 
-        for (String word : splitLine){
-            for (int i = 0; i < trackList.size() - 1; i++){
-                nextScores = new HashMap<>();
-                nextStates = new HashSet<>();
+        for (int i = 0; i < splitLine.length - 1; i++){
+            nextScores = new HashMap<>();
+            nextStates = new HashSet<>();
 
-                for (String currState : currStates){
-                    for (String transition : BuildModel.getTransitions().get(currState).keySet()){
-                        nextStates.add(transition);
-                        double nextScore = 0;
-                        nextScore += BuildModel.getTransitions().get(currState).get(transition);
-                        nextScore += currScores.get(currState);
-                        if (!BuildModel.getObservations().get(currState).containsKey(word)){
-                            nextScore += unseenScore;
-                        }
-                        else {
-                            nextScore += BuildModel.getObservations().get(currState).get(word);
-                        }
-
-                        if (!nextScores.containsKey(transition) || nextScore > nextScores.get(transition)){
-                            nextScores.put(transition, nextScore);
-
-                        }
-                        currStates = nextStates;
-                        currScores = nextScores;
+            for (String currState : currStates){
+                for (String transition : BuildModel.getTransitions().get(currState).keySet()){
+                    nextStates.add(transition);
+                    double nextScore = 0;
+                    nextScore += BuildModel.getTransitions().get(currState).get(transition);
+                    nextScore += currScores.get(currState);
+                    if (!BuildModel.getObservations().get(currState).containsKey(splitLine[i])){
+                        nextScore += unseenScore;
                     }
+                    else {
+                        nextScore += BuildModel.getObservations().get(currState).get(splitLine[i]);
+                    }
+
+                    if (!nextScores.containsKey(transition) || nextScore > nextScores.get(transition)){
+                        nextScores.put(transition, nextScore);
+                        track.put(transition, currState);
+                    }
+                    currStates = nextStates;
+                    currScores = nextScores;
                 }
+
+            }
+            trackList.add(track); //add observation per word
+        }
+
+        //for each unique next state
+        for (String s : trackList.get(trackList.size() - 1).keySet()){
+            double max = -1;
+            String state = "";
+            if (currScores.get(s) > max){
+                max = currScores.get(s);
+                state = s;
             }
         }
 
 
 
-        String[] splitLine = line.split(" ");
+
+
+        //String[] splitLine = line.split(" ");
         String[] sentence = new String[splitLine.length + 1];
         for (int i = 1; i < splitLine.length; i++){
             sentence[i] = splitLine[i - 1];
