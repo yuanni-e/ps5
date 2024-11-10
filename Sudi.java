5bd8686019010c3a66f9d2fa864a3eea9b3d87e7
claire601 <clacha24@bergen.org>
2024-11-10 09:50:13 -0500

    it's all about the family and it's all about the BOOOOOM

--- Sudi.java
+++ Sudi.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Sudi {
-    private final int unseenScore = -100;
+    private final int unseenScore = -10;
     String start = "#";
 
     private Map<String, Double> currScores;
@@ -12,8 +12,6 @@ public class Sudi {
     private List<Map<String, String>> trackList;
     private List<String> backtrace;
 
-    private Scanner in = new Scanner(System.in);
-
     public Sudi(){
         currScores = new HashMap<>();
         currStates = new HashSet<>();
@@ -22,9 +20,8 @@ public class Sudi {
         backtrace = new ArrayList<>();
     }
 
-    public void POSViterbi(String line){
+    public void POSViterbi(String line, Map<String, Map<String, Double>> transitions, Map<String, Map<String, Double>> observations){
         String[] splitLine = line.split(" ");
-
         currStates.add(start);
         currScores.put(start, 0.0);
 
@@ -34,39 +31,59 @@ public class Sudi {
 
             for (String currState : currStates){ //for each current state
                 //for each next state (current state to each next state: transition)
-                for (String nextState : BuildModel.getTransitions().get(currState).keySet()){
-                    nextStates.add(nextState); //add next state
-                    double nextScore = 0;
-                    //add transition score
-                    nextScore += BuildModel.getTransitions().get(currState).get(nextState);
-                    //add score corresponding to curr state
-                    nextScore += currScores.get(currState);
+                System.out.println(splitLine[i]);
+                System.out.println("currState: " + currState);
+                if (transitions.get(currState) != null){
+                    for (String nextState : transitions.get(currState).keySet()){
+                        nextStates.add(nextState); //add next state
+                        double nextScore = 0;
 
-                    //if curr state observations don't have an instance of the word
-                    if (!BuildModel.getObservations().get(currState).containsKey(splitLine[i])){
-                        nextScore += unseenScore; //add unseen score
-                    }
-                    else {
-                        //add score corresponding to word
-                        nextScore += BuildModel.getObservations().get(currState).get(splitLine[i]);
-                    }
+                        System.out.println("nextState: " + nextState);
+                        //add score corresponding to curr state
+                        nextScore += currScores.get(currState);
+                        System.out.print(currScores.get(currState) + "(curr)");
 
-                    //if there is no score for next state or the score now is greater than the previous one for next state
-                    if (!nextScores.containsKey(nextState) || nextScore > nextScores.get(nextState)){
-                        //add next state and score
-                        nextScores.put(nextState, nextScore);
-                        //keep track of next state and state it came from
-                        track.put(nextState, currState);
+                        //add transition score
+                        nextScore += transitions.get(currState).get(nextState);
+                        System.out.print(" + " + transitions.get(currState).get(nextState) + "(tran)");
+                        //if curr state observations don't have an instance of the word
+                        //if (observations.get(currState) != null){
+                            if (!observations.get(nextState).containsKey(splitLine[i])){
+                                nextScore += unseenScore; //add unseen score
+                                System.out.print(" + " + unseenScore + "(obs)");
+                            }
+                            else {
+                                //add score corresponding to word
+                                nextScore += observations.get(nextState).get(splitLine[i]);
+                                System.out.print(" + " + observations.get(nextState).get(splitLine[i]) + "(obs)");
+                            }
+                        //}
+                        System.out.println(nextState + ": " + nextScore);
+
+                        //if there is no score for next state or the score now is greater than the previous one for next state
+                        if (!nextScores.containsKey(nextState) || nextScore > nextScores.get(nextState)){
+                            //add next state and score
+                            nextScores.put(nextState, nextScore);
+                            //keep track of next state and state it came from
+                            track.put(nextState, currState);
+                            //`System.out.println(currState);
+                            System.out.println(track);
+                        }
                     }
-                    currStates = nextStates; //next states are used for new current
-                    currScores = nextScores; //next scores are used for new current
+                    //System.out.println("done");
                 }
             }
+            System.out.println("done2");
+            currStates = nextStates; //next states are used for new current
+            currScores = nextScores; //next scores are used for new current
+            System.out.println("currScores: " + currScores);
             trackList.add(track); //add observation per word in line
+            track = new HashMap<>();
         }
+        System.out.println("trackList: " + trackList);
 
         String state = "";
-        double max = -1;
+        double max = -1000000;
         //for each unique next state for the last word
         for (String s : trackList.get(trackList.size() - 1).keySet()){
             if (currScores.get(s) > max){
@@ -77,10 +94,15 @@ public class Sudi {
         backtrace.add(state); //add state with highest score
         for (int i = trackList.size() - 1; i >= 0; i--){
             //add the current state that corresponds to next state
-            backtrace.add(trackList.get(i).get(state));
+            backtrace.add(0, trackList.get(i).get(state));
             //set state to current state
             state = trackList.get(i).get(state);
         }
+        System.out.println("backtrace: " + backtrace);
+    }
+
+    public static void main(String[] args) {
+        //Sudi test = new Sudi();
     }
 
 
