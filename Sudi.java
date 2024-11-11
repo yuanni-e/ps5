7be174844c0bed43a844e6a6e3a63ec25732d911
claire601 <clacha24@bergen.org>
2024-11-11 08:07:04 -0500

    comments

--- Sudi.java
+++ Sudi.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -5,14 +7,11 @@ import java.util.Scanner;
 
 public class Sudi {
     private Scanner in;
-    //training materials are default set to brown but you can change them if you want daniel
-    private String trainSentences = "texts/brown-train-sentences.txt";
-    private String trainTags = "texts/brown-train-tags.txt";
     private Map<String, Map<String, Double>> transitionMap;
     private Map<String, Map<String, Double>> observationMap;
     private Viterbi viterbi;
 
-    public Sudi(){
+    public Sudi(String trainSentences, String trainTags){
         try{
             BuildModel viterbiModel = new BuildModel();
             viterbi = new Viterbi();
@@ -24,26 +23,80 @@ public class Sudi {
         }
     }
 
-    public void consoleTest() throws Exception{
+    /**
+     * Allows user to input a sentence to be tagged
+     * @throws Exception
+     */
+    public void consoleTest() throws Exception {
         in = new Scanner(System.in);
-        System.out.println("Enter a sentence to tag: ");
+        System.out.println("Enter a sentence to tag:");
         String input = in.nextLine(); //gets user's keyboard input
-        System.out.println(input);
-        List<String> tags = viterbi.POSViterbi(input, transitionMap, observationMap);
-        System.out.println(tags);
-        String[] words = input.split(" ");
-        String taggedSentence = "";
-        for(int i = 0; i < words.length; i++){
-            taggedSentence += words[i] + "/" + tags.get(i+1) + "  ";
+        while(!input.equals("no")) { //enter "No" or "NO" or "nO" if you want to tag it on its own
+            System.out.println(input);
+            viterbi = new Viterbi();
+            List<String> tags = viterbi.POSViterbi(input, transitionMap, observationMap);
+
+            String[] words = input.split(" ");
+            String taggedSentence = "";
+            for (int i = 0; i < words.length; i++) {
+                taggedSentence += words[i] + "/" + tags.get(i + 1) + "  ";
+            }
+            System.out.println("Here is your tagged sentence: " + taggedSentence);
+            System.out.println("Would you like to tag another sentence? (enter \"no\" to stop, enter anything else to continue tagging)");
+            input = in.nextLine();
+            if(!input.equals("no")) {
+                System.out.println("Enter a sentence to tag:");
+                input = in.nextLine();
+            }
         }
-        System.out.println("Here is your tagged sentence: " + taggedSentence);
         in.close();
     }
 
+    public void fileTest(String sentenceFile, String tagFile) throws IOException{
+        int wrong = 0;
+        BufferedReader sentences = new BufferedReader(new FileReader(sentenceFile));
+        BufferedReader tags = new BufferedReader(new FileReader(tagFile));
+        String sentence = sentences.readLine();
+        String tagline = tags.readLine();
+
+        while (sentence != null){
+            Viterbi test = new Viterbi();
+            List<String> testTagLine = test.POSViterbi(sentence, transitionMap, observationMap);
+            String[] testTags = tagline.split(" ");
+            String[] words = sentence.split(" ");
+            for (int i = 0; i < testTagLine.size()-1; i++){
+                String tag = testTagLine.get(i+1);
+                if(!tag.equals(testTags[i])){
+                    wrong++;
+                }
+                System.out.print(words[i] + "/" + testTagLine.get(i + 1) + "  ");
+            }
+            System.out.println();
+            sentence = sentences.readLine();
+            tagline = tags.readLine();
+        }
+        System.out.println("Wrong tags: " + wrong);
+    }
+
     public static void main(String[] args) throws Exception{
-        Sudi tester = new Sudi();
+        Sudi simpleTester = new Sudi("texts/simple-train-sentences.txt", "texts/simple-train-tags.txt");
+        simpleTester.fileTest("texts/simple-test-sentences.txt", "texts/simple-test-tags.txt");
+
+        Sudi brownTester = new Sudi("texts/brown-train-sentences.txt", "texts/brown-train-tags.txt");
+        brownTester.fileTest("texts/brown-test-sentences.txt", "texts/brown-test-tags.txt");
         try {
-            tester.consoleTest();
+            brownTester.consoleTest();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Sudi fileTester = new Sudi("texts/simple-train-sentences.txt", "texts/simple-train-tags.txt");
+        fileTester.fileTest("texts/simple-test-sentences.txt", "texts/simple-test-tags.txt");
+
+        brownTester.fileTest("texts/example-sentences.txt", "texts/example-tags.txt");
+
+        Sudi boomTester = new Sudi("texts/boom-train-sentences.txt", "texts/boom-train-tags.txt");
+        try {
+            boomTester.consoleTest();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- Viterbi.java
+++ Viterbi.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class Viterbi {
-    private static final double unseenScore = -100.0;
-    static String start = "#";
+    private final double unseenScore = -100.0; //set unseen score
+    static String start = "#"; //start state that transitions to first word of the line
 
-    private static Map<String, Double> currScores;
-    private static Map<String, Double> nextScores;
-    private static Set<String> currStates;
-    private static Set<String> nextStates;
-    private static Map<String, String> track;
-    private static List<Map<String, String>> trackList;
-    private static List<String> backtrace;
+    private Map<String, Double> currScores; //current state maps to its score
+    private Map<String, Double> nextScores; //next state maps to its score
+    private Set<String> currStates; //keep track of current states
+    private Set<String> nextStates; //keep track of next states (after transitions)
+    private Map<String, String> track; //next state maps to prev state; prev state is value since there can be duplicate values in a map
+    private List<Map<String, String>> trackList; //list of tracks
+    private List<String> backtrace; //backtrace to find most likely tags
 
     public Viterbi(){
         currScores = new HashMap<>();
@@ -20,78 +20,63 @@ public class Viterbi {
         backtrace = new ArrayList<>();
     }
 
-    public static List<String> POSViterbi(String line, Map<String, Map<String, Double>> transitions, Map<String, Map<String, Double>> observations){
+    /**
+     * Performs Viterbi algorithm on a sentence
+     * @param line to be read in
+     * @param transitions a map from current tag to a map of next tags and scores
+     * @param observations a map from current tag to a map of words corresponding to tag and scores
+     * @return backtrace list
+     */
+    public List<String> POSViterbi(String line, Map<String, Map<String, Double>> transitions, Map<String, Map<String, Double>> observations){
 
-        String[] splitLine = line.split(" ");
-        currStates.add(start);
-        currScores.put(start, 0.0);
+        String[] splitLine = line.split(" "); //take in the line, split into array entries upon 'space'
+        currStates.add(start); //add start state to currStates
+        currScores.put(start, 0.0); //put a score of 0.0 for start in currScores
 
-
-        for (int i = 0; i < splitLine.length; i++){ //for each word
+        for (int i = 0; i < splitLine.length; i++){ //for each word in line
             nextScores = new HashMap<>();
             nextStates = new HashSet<>();
 
-            for (String currState : currStates){ //for each current state
-                //for each next state (current state to each next state: transition)
+            for (String currState : currStates){ //for each curr state in currStates set
                 if (transitions.get(currState) != null){
+                    //for each next state of current state
                     for (String nextState : transitions.get(currState).keySet()){
-                        //System.out.println(nextState);
-                        nextStates.add(nextState); //add next state
+                        nextStates.add(nextState); //add next state to set
                         double nextScore = 0.0;
 
                         //add score corresponding to curr state
                         nextScore += currScores.get(currState);
-                        //System.out.print(currScores.get(currState) + "(curr) + ");
 
-                        //add transition score
+                        //add the transition score between curr state and next state
                         nextScore += transitions.get(currState).get(nextState);
-                        //System.out.print(transitions.get(currState).get(nextState) + "(tran) + ");
-                        //if curr state observations don't have an instance of the word
+
+                        //if next state observations do not have an instance of word
                         if (!observations.get(nextState).containsKey(splitLine[i])){
                             nextScore += unseenScore; //add unseen score
-                            //System.out.println(unseenScore + "(obs)");
                         }
-                        else {
-                            //add score corresponding to word
+                        else { //word has been observed in next state
+                            //add score corresponding to observation of word
                             nextScore += observations.get(nextState).get(splitLine[i]);
-                            //System.out.println(observations.get(nextState).get(splitLine[i]) + "(obs)");
                         }
 
-                        //if there is no score for next state or the score now is greater than the previous one for next state
+                        //if there is no score already for next state or the score now is greater than the previous one for next state
                         if (!nextScores.containsKey(nextState) || nextScore > nextScores.get(nextState)){
-                            //add next state and score
-                            nextScores.put(nextState, nextScore);
-                            //keep track of next state and state it came from
-                            track.put(nextState, currState);
+                            nextScores.put(nextState, nextScore); //add next state and its score to nextScores map
+                            track.put(nextState, currState); //put next state and curr state in track map
                         }
                     }
                 }
             }
-            currStates = nextStates; //next states are used for new current
-            currScores = nextScores; //next scores are used for new current
-            trackList.add(track); //add observation per word in line
-            track = new HashMap<>();
+            currStates = nextStates; //use nextStates as currStates for next iteration
+            currScores = nextScores; //use nextScores as currScores for next iteration
+            trackList.add(track);
+            track = new HashMap<>(); //reset track for new word
         }
 
         String state = null;
-        //for each unique next state for the last word
+        //for each state in currScores (scores from the last word in line)
         for (String s : currScores.keySet()){
-//            if (currScores.get(s) > max){
-//                max = currScores.get(s);
-//                state = s; //state with the highest score
-//                // if state is null set it to s
-//                // otherwise check if s's score is better than state's score
-//                //      if so, set state to s
-//            }
-            // {N: 10, P= 15, NP = 2}
-            // state = null
-            // first round: s=N
-            //state = N
-            // second round: s=P
-            // state = P
-            // third round: s=NP
-            // state is still P
-            // -> max state is P!
+            //find state with the highest score
             if (state == null){
                 state = s;
             }
@@ -99,17 +84,14 @@ public class Viterbi {
                 state = s;
             }
         }
-        backtrace.add(state); //add state with highest score
+        backtrace.add(state); //add state with the highest score to backtrace
         for (int i = trackList.size() - 1; i >= 0; i--){
-            //add the current state that corresponds to next state
+            //add the prev state of state
             backtrace.add(0, trackList.get(i).get(state));
-            //set state to current state
+            //set state to prev state
             state = trackList.get(i).get(state);
         }
-        //System.out.println("backtrace: " + backtrace);
 
         return backtrace;
     }
-
-
 }
